a011973087b6656de39cd45a0b8eb5b64e34228b
Mariya <mbeketova@rambler.ru>
2015-04-05 20:14:50 +0300

    LessonThree

--- LessonThree_V2/ViewController.m
+++ LessonThree_V2/ViewController.m
@@ -10,14 +10,69 @@
 
 @interface ViewController ()
 
+@property(nonatomic, strong) NSArray * arrayPrices;
+@property(nonatomic, strong) NSArray * arrayValues;
+@property(nonatomic, strong) NSMutableArray * arrayM;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    NSString * stringPrices = @"123,1020,500";
+    NSString * stringValues = @"Тетрадь,Телевизор,Утюг";
+    NSMutableArray * arrayM = [[NSMutableArray alloc]init];
+    
+    
+    self.arrayPrices = [stringPrices componentsSeparatedByString:@","];
+    self.arrayValues = [stringValues componentsSeparatedByString:@","];
+
+    
+    for (int i = 0; i < self.arrayPrices.count; i++) {
+        NSDictionary * dict = [[NSDictionary alloc] initWithObjectsAndKeys:
+                               [self.arrayPrices objectAtIndex:i], @"prices",
+                               [self.arrayValues objectAtIndex:i], @"values", nil];
+        
+        
+        [arrayM addObject:dict];
+        
+    }
+
+    NSLog(@"%@", arrayM);
+    
+    
 }
+
+#pragma mark - UITableViewDelegate
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section; {
+    
+    return self.arrayPrices.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    static NSString *simpleTableIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:simpleTableIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
+    }
+    
+    cell.textLabel.text = [[self.arrayM objectAtIndex:indexPath.row]objectForKey:@"values"];
+    cell.detailTextLabel.text = [[self.arrayM objectAtIndex:indexPath.row]objectForKey:@"prices"];
+    return cell;
+}
+
+
+
+- (void)tableView:(UITableView*)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    
+}
+
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
