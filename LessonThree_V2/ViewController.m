2685dc08616ecd7bb7ff18042c948eae6b4fbb16
Mariya <mbeketova@rambler.ru>
2015-04-05 18:09:22 +0300

    Initial Commit

---
+++ LessonThree_V2/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LessonThree_V2
+//
+//  Created by Admin on 05.04.15.
+//  Copyright (c) 2015 Mariya Beketova. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
